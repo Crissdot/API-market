b35980e3ede40f7b55628324611d1d6683bf6898
crissdot <crissdotdev@gmail.com>
2022-10-04 21:43:53 -0500

    add product and purchase entities

---
+++ src/main/java/com/course/market/persistence/entity/Product.java
@@ -0,0 +1,35 @@
+package com.course.market.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "productos")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProduct;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "id_categoria")
+    private Integer idCategory;
+
+    @Column(name = "codigo_barras")
+    private String barCode;
+
+    @Column(name = "precio_venta")
+    private Double salePrice;
+
+    @Column(name = "cantidad_stock")
+    private Integer stock;
+
+    @Column(name = "estado")
+    private Boolean state;
+}
---
+++ src/main/java/com/course/market/persistence/entity/Purchase.java
@@ -0,0 +1,33 @@
+package com.course.market.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "compras")
+public class Purchase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idPurchase;
+
+    @Column(name = "id_cliente")
+    private String idClient;
+
+    @Column(name = "fecha")
+    private LocalDateTime date;
+
+    @Column(name = "medio_pago")
+    private String paymentMethod;
+
+    @Column(name = "comentario")
+    private String comment;
+
+    @Column(name = "estado")
+    private String state;
+}
