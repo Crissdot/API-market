b5598978b5ad5c7239cf31270e43982ed25ec9fa
crissdot <crissdotdev@gmail.com>
2022-10-05 22:06:07 -0500

    add query methods to product repository

--- src/main/java/com/course/market/persistence/crud/ProductCrudRepository.java
+++ src/main/java/com/course/market/persistence/crud/ProductCrudRepository.java
@@ -1,8 +1,19 @@
 package com.course.market.persistence.crud;
 
 import com.course.market.persistence.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
 
+//    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+//    List<Product> getByCategoryId (Integer idCategory);
+//    List<Product> findByIdCategory (Integer idCategory);
+
+    List<Product> findByIdCategoryOrderByNameAsc (Integer idCategory);
+
+    Optional<List<Product>> findByStockLessThanAndState (Integer stock, Boolean state);
 }
--- src/main/java/com/course/market/persistence/repository/ProductRepository.java
+++ src/main/java/com/course/market/persistence/repository/ProductRepository.java
@@ -5,11 +5,20 @@ import com.course.market.persistence.entity.Product;
 import org.apache.commons.collections4.IterableUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll() {
         return IterableUtils.toList(productCrudRepository.findAll());
+    }
+
+    public List<Product> getByCategory (Integer idCategory) {
+        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    }
+
+    public Optional<List<Product>> getScarce (Integer quantity) {
+        return productCrudRepository.findByStockLessThanAndState(quantity, true);
     }
 }
