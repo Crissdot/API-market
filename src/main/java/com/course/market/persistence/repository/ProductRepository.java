4dde65c49f240ee18ee5d5fea918b6dba53c6824
crissdot <crissdotdev@gmail.com>
2022-10-05 22:28:58 -0500

    rename repository annotation to product

--- src/main/java/com/course/market/persistence/repository/ProductRepository.java
+++ src/main/java/com/course/market/persistence/repository/ProductRepository.java
@@ -3,11 +3,15 @@ package com.course.market.persistence.repository;
 import com.course.market.persistence.crud.ProductCrudRepository;
 import com.course.market.persistence.entity.Product;
 import org.apache.commons.collections4.IterableUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
+    @Autowired
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll() {
@@ -20,5 +24,17 @@ public class ProductRepository {
 
     public Optional<List<Product>> getScarce (Integer quantity) {
         return productCrudRepository.findByStockLessThanAndStatus(quantity, true);
+    }
+
+    public Optional<Product> getProduct (Integer idProduct) {
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product createProduct (Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void deleteProduct (Integer idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
