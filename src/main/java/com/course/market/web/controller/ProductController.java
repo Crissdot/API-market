96f24051767b8f7cc3dccb044e1f5e806a39a965
crissdot <crissdotdev@gmail.com>
2022-10-10 19:51:11 -0500

    add swagger documentation

--- src/main/java/com/course/market/web/controller/ProductController.java
+++ src/main/java/com/course/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.course.market.web.controller;
 
 import com.course.market.domain.ProductDomain;
 import com.course.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,31 +21,58 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<ProductDomain>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<ProductDomain> getProduct(@PathVariable Integer productId) {
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found"),
+    })
+    public ResponseEntity<ProductDomain> getProduct(
+            @ApiParam(value = "The id of the product", required = true, example = "1") @PathVariable Integer productId
+    ) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<ProductDomain>> getByCategory(@PathVariable Integer categoryId) {
+    @ApiOperation("Search a product with a category")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found"),
+    })
+    public ResponseEntity<List<ProductDomain>> getByCategory(
+            @ApiParam(value = "The id category of the product", required = true, example = "1") @PathVariable Integer categoryId
+    ) {
         return productService.getByCategory(categoryId)
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public ResponseEntity<ProductDomain> create(@RequestBody ProductDomain productDomain) {
+    @ApiOperation("Create a new product")
+    @ApiResponse(code = 201, message = "Created")
+    public ResponseEntity<ProductDomain> create(
+            @ApiParam(value = "The content of the product", required = true) @RequestBody ProductDomain productDomain
+    ) {
         return new ResponseEntity<>(productService.create(productDomain), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{productId}")
-    public ResponseEntity delete(@PathVariable Integer productId) {
+    @ApiOperation("Delete a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found"),
+    })
+    public ResponseEntity delete(
+            @ApiParam(value = "The id of the product", required = true, example = "1") @PathVariable Integer productId
+    ) {
         if (productService.delete(productId)) {
             return new ResponseEntity(HttpStatus.OK);
         }
--- src/main/java/com/course/market/web/controller/PurchaseController.java
+++ src/main/java/com/course/market/web/controller/PurchaseController.java
@@ -2,6 +2,10 @@ package com.course.market.web.controller;
 
 import com.course.market.domain.PurchaseDomain;
 import com.course.market.domain.service.PurchaseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,19 +20,32 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping
+    @ApiOperation("Get all purchases")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<PurchaseDomain>> getAll() {
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/client/{idClient}")
-    public ResponseEntity<List<PurchaseDomain>> getByClient(@PathVariable("idClient") String clientId) {
+    @ApiOperation("Search a purchase with client ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found"),
+    })
+    public ResponseEntity<List<PurchaseDomain>> getByClient(
+            @ApiParam(value = "The id of the client", required = true, example = "1") @PathVariable("idClient") String clientId
+    ) {
         return purchaseService.getByClient(clientId).map(
                 purchases -> new ResponseEntity<>(purchases, HttpStatus.OK)
         ).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping
-    public ResponseEntity<PurchaseDomain> create(@RequestBody PurchaseDomain purchase) {
+    @ApiOperation("Create a new purchase")
+    @ApiResponse(code = 201, message = "Created")
+    public ResponseEntity<PurchaseDomain> create(
+            @ApiParam(value = "The content of the purchase", required = true) @RequestBody PurchaseDomain purchase
+    ) {
         return new ResponseEntity<>(purchaseService.create(purchase), HttpStatus.CREATED);
     }
 }
