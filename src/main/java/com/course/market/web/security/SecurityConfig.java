72499094c91bf9eced78118b7e07f9aa0aff9a2a
crissdot <crissdotdev@gmail.com>
2022-10-10 20:31:29 -0500

    add spring security filter chain

---
+++ src/main/java/com/course/market/web/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.course.market.web.security;
+
+import com.course.market.domain.service.UserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private UserDetailService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic().and().authenticationManager(authenticationManager);
+
+        return http.build();
+    }
+}
