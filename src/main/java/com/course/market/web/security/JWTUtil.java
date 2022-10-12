00a49b7043780d9b5ebb0fbce1e02fe5da07af62
crissdot <crissdotdev@gmail.com>
2022-10-12 18:36:58 -0500

    add jwt

---
+++ src/main/java/com/course/market/web/security/JWTUtil.java
@@ -0,0 +1,21 @@
+package com.course.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+    private static final Integer TEN_HOURS_IN_MILLIS = 1000 * 60 * 60 * 10;
+    private static final String SECRET_KEY = "test";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + TEN_HOURS_IN_MILLIS))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
